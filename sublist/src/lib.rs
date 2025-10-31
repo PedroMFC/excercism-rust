6937f194e4402a5f3a50a9cecf4c97367b7ef770
Pedro Flores <pedro_23_96@hotmail.com>
2025-10-31 20:29:10 +0100

    feat: sublist using window function

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -33,3 +33,28 @@ pub fn sublist(first_list: &[i32], second_list: &[i32]) -> Comparison {
     }
     result
 }
+
+// USING window
+// #[derive(Debug, PartialEq, Eq)]
+// pub enum Comparison {
+//     Equal,
+//     Sublist,
+//     Superlist,
+//     Unequal,
+// }
+// pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+//     let superlist = second_list.is_empty()
+//         || first_list
+//             .windows(second_list.len())
+//             .any(|x| x == second_list);
+//     let sublist = first_list.is_empty()
+//         || second_list
+//             .windows(first_list.len())
+//             .any(|x| x == first_list);
+//     match (superlist, sublist) {
+//         (true, true) => Comparison::Equal,
+//         (true, false) => Comparison::Superlist,
+//         (false, true) => Comparison::Sublist,
+//         (false, false) => Comparison::Unequal,
+//     }
+// }
