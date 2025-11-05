3f1d24ef49cd2a09dcd2980c10e4454c975f7414
Pedro Flores <pedro_23_96@hotmail.com>
2025-11-05 19:41:52 +0100

    enh: bob with trim

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn reply(message: &str) -> &str {
     let is_uppercase = message.chars().filter(|c| c.is_alphanumeric()).all(|c| c.is_uppercase() || c.is_numeric()) && message.chars().any(|c| c.is_alphabetic());
-    match message.chars().filter(|c| !c.is_whitespace()).collect::<String>().ends_with('?') {
+    match message.trim().ends_with('?') {
         true =>  match is_uppercase{
             true => "Calm down, I know what I'm doing!",
             false => "Sure.",
         },
         false => match is_uppercase {
             true => "Whoa, chill out!",
-            false => match message.chars().filter(|c| !c.is_whitespace()).collect::<String>().is_empty() {
+            false => match message.trim().is_empty() {
                 true => "Fine. Be that way!",
                 false => "Whatever.",
             },
